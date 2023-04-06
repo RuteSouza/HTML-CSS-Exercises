73579aa7bed9d28f8a70e8aeddd30cd81b95839d
Rute Souza <75092581+RuteSouza@users.noreply.github.com>
2023-04-06 03:00:02 -0300

    Create script.js

---
+++ Lamp on and off/script.js
@@ -0,0 +1,10 @@
+function changeImage(){
+  var image = document.getElementById ('myImage');
+  if (image.src.match("lampon")){
+    image.src ="assets/lampoff.jpg";
+  }
+  else{
+    image.src="assets/lampon.jpg";
+  }
+  
+}
